446a845932b327e3841a5942625ba90f7250e27e
Feist, James <james.feist@intel.com>
2017-08-29 11:10:39 -0700

    Add Thresholds to Fan Sensor
    Add thresholds to fan sensor and read information from
    JSON. Also some minor enhancements like slowing down the sensor.
    
    Change-Id: Icda6e40c4acc88c5e96cc392d7f94968484bad0e

--- sensors/fans/FanSensor.cpp
+++ sensors/fans/FanSensor.cpp
@@ -1,4 +1,8 @@
+#include <limits.h>
 #include <unistd.h>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <dbus/connection.hpp>
 #include <dbus/endpoint.hpp>
 #include <dbus/message.hpp>
@@ -9,11 +13,10 @@
 #include <regex>
 #include <string>
 
-
 #define JSON_STORE "/var/configuration/sensors.json"
+#define PWM_POLL_MS 500
 
 namespace fs = std::experimental::filesystem;
-int sensor_count = 0;  // todo move this elseware
 
 bool find_files(const fs::path dir_path, const std::string& match_string,
                 std::vector<fs::path>& found_paths,
@@ -25,8 +28,8 @@ bool find_files(const fs::path dir_path, const std::string& match_string,
   std::smatch match;
   for (auto& p : fs::recursive_directory_iterator(dir_path)) {
     std::string path = p.path().string();
-    if (!is_directory(p)){
-      if(std::regex_search(path, match, search))
+    if (!is_directory(p)) {
+      if (std::regex_search(path, match, search))
         found_paths.emplace_back(p.path());
     }
     // since we're using a recursve iterator, these should only be symlink dirs
@@ -37,61 +40,20 @@ bool find_files(const fs::path dir_path, const std::string& match_string,
   return true;
 }
 
-std::string find_name(const std::string& dir_path, int count) {
-  fs::path p(dir_path);
-  fs::path dir = p.parent_path();
-  std::string of_node = dir.string() + "/of_node";
-  std::vector<fs::path> oemfiles;
-  find_files(of_node, "oemname\\d+", oemfiles);
-  std::regex search("fan(\\d+)_input");
-  std::smatch match;
-  int index = -1;
-  int ii = 0;
-  bool b_sensor = false;
-  std::string name;
-
-  if (!std::regex_search(dir_path, match, search))
-    return std::string("unknown");
-
-  for (auto x : match) {
-    if (ii == 0)
-      ii++;
-    else {
-      index = std::stoi(x);
-      break;
-    }
-  }
-
-  if (index == -1) return std::string("unknown");
-
-  if (oemfiles.size() < count) {
-    if (index % 2 == 0)
-      b_sensor = true;  // todo(james) will we ever have 'c' sensors?
-    index = (index + 1) / 2;
-  }
-  std::ifstream namefile(of_node + std::string("/oemname") +
-                         std::to_string(index));
-  if(!namefile.good())
-   return std::string("unknown");
-
-  std::getline(namefile, name);
-  namefile.close();
-  name.pop_back();
-  if (b_sensor) name.append("b");
-  return name;
-}
-
 class Tach {
  public:
   Tach(std::string& path, std::shared_ptr<dbus::connection> system_bus,
-       boost::asio::io_service& io)
+       boost::asio::io_service& io, std::string& fan_name,
+       nlohmann::json& json_config)
       : path(path),
         system_bus(system_bus),
         io(io),
-        name(find_name(path, sensor_count)),
+        name(boost::replace_all_copy(fan_name, " ", "_")),
+        json_config(json_config),
         dbus_endpoint("", "/xyz/openbmc_project/sensors/tach/" + name,
                       "org.freedesktop.Dbus.Properties"),
-        input_dev(io, open(path.c_str(), O_RDONLY)) {
+        input_dev(io, open(path.c_str(), O_RDONLY)),
+        wait_timer(io) {
     setup_read();
   }
 
@@ -100,21 +62,26 @@ class Tach {
   std::shared_ptr<dbus::connection> system_bus;
   boost::asio::io_service& io;
   std::string name;
+  nlohmann::json& json_config;
   dbus::endpoint dbus_endpoint;
   boost::asio::posix::stream_descriptor input_dev;
+  boost::asio::deadline_timer wait_timer;
   boost::asio::streambuf read_buf;
 
-  float value = 0;
+  float value =
+      FLT_MAX;  // max so we don't get a threshold uncrossed event at start
+  int err_count = 0;
 
   void setup_read(void) {
     boost::asio::async_read_until(
         input_dev, read_buf, '\n',
         [&](const boost::system::error_code& ec,
-               std::size_t /*bytes_transfered*/) { handle_response(ec); });
+            std::size_t /*bytes_transfered*/) { handle_response(ec); });
   }
 
   void handle_response(const boost::system::error_code& err) {
     if (!err) {
+      err_count = 0;
       std::istream response_stream(&read_buf);
       std::string response;
       std::getline(response_stream, response);
@@ -124,14 +91,53 @@ class Tach {
     } else {
       // todo(james) this happens mostly on poweroff, where we shouldn't be
       // reading anyway
+
       std::cerr << "Failure to read sensor " << name << " at " << path << "\n";
+
+      if (err_count >= 10) {
+        // only send value update once
+        if (err_count == 10) {
+          update_value(0);
+          err_count++;
+        }
+      } else
+        err_count++;
     }
     input_dev.close();
     input_dev.assign(open(path.c_str(), O_RDONLY));
-    setup_read();
+    wait_timer.expires_from_now(boost::posix_time::milliseconds(PWM_POLL_MS));
+    wait_timer.async_wait(
+        [&](const boost::system::error_code&) { setup_read(); });
+  }
+
+  void check_thresholds(float new_value) {
+    int index = boost::ends_with(name, "b") ? 1 : 0;
+    auto thresholds = json_config["thresholds"].at(index);
+    for (auto& threshold : thresholds) {
+      if (threshold["direction"] == "less than") {
+        if (value >= threshold["value"] && new_value < threshold["value"]) {
+          std::cout << "threshold crossed!\n";  // todo(james) assert on dbus
+        } else if (value < threshold["value"] &&
+                   new_value >= threshold["value"]) {
+          std::cout << "threshold uncrossed!\n";
+        }
+      } else if (threshold["direction"] == "greater than") {
+        if (value <= threshold["value"] && new_value > threshold["value"]) {
+          std::cout << "threshold crossed!\n";  // todo(james) assert on dbus
+        } else if (value > threshold["value"] &&
+                   new_value <= threshold["value"]) {
+          std::cout << "threshold uncrossed!\n";
+        }
+
+      } else {
+        std::cerr << "Invalid threshold " << threshold["direction"] << ".\n";
+      }
+    }
   }
 
   void update_value(float new_value) {
+    check_thresholds(new_value);
+
     auto signal_name = std::string("PropertiesChanged");
     auto m = dbus::message::new_signal(dbus_endpoint, signal_name);
     value = new_value;
@@ -147,17 +153,17 @@ class Tach {
   }
 };
 
-nlohmann::json parse_json(void){
- std::ifstream f(JSON_STORE);
- nlohmann::json input;
- nlohmann::json ret;
- input << f;
- for(auto it = input.begin(); it != input.end(); it++){
-  auto value = it.value();
-  if(value["type"] == "AspeedFan"){
-   ret.emplace_back(value);
+nlohmann::json parse_json(void) {
+  std::ifstream f(JSON_STORE);
+  nlohmann::json input;
+  nlohmann::json ret;
+  input << f;
+  for (auto it = input.begin(); it != input.end(); it++) {
+    auto value = it.value();
+    if (value["type"] == "AspeedFan") {
+      ret.emplace_back(value);
+    }
   }
- }
   return ret;
 }
 
@@ -166,25 +172,47 @@ int main(int argc, char** argv) {
   auto system_bus = std::make_shared<dbus::connection>(io, dbus::bus::system);
 
   std::vector<std::string> tach_paths;
-  nlohmann::json j = parse_json();
-  tach_paths.reserve(20); //systems should have less than 20 fans
+  nlohmann::json fan_json = parse_json();
+  tach_paths.reserve(20);  // systems should have less than 20 fans
   // create tachs
-  for(auto& fan : j){
-       for(auto tach : *fan.find("tachs")){
-        std::vector<fs::path> paths;
-        if(find_files(fs::path("/sys/class/hwmon"), "fan" + std::to_string(tach.get<int>()) + "_input", paths)){
-         for(const auto& path : paths){  // todo, should only be 1
-           tach_paths.emplace_back(path.string());
-          }
+  for (auto& fan : fan_json) {
+    auto connector = fan["connector"];
+    for (auto tach : *connector.find("tachs")) {
+      std::vector<fs::path> paths;
+      if (find_files(fs::path("/sys/class/hwmon"),
+                     "fan" + std::to_string(tach.get<int>()) + "_input",
+                     paths)) {
+        for (const auto& path : paths) {  // todo, should only be 1
+          tach_paths.emplace_back(path.string());
         }
       }
-
+    }
   }
-  sensor_count = tach_paths.size();
+
+  if (!fan_json.size()) {
+    std::cerr << "No records found in configuration\n";
+    return 1;
+  }
+
+  int tach_per_fan = tach_paths.size() / fan_json.size();
+  std::cout << tach_per_fan << " tach(s) per fan found.\n";
 
   std::vector<std::unique_ptr<Tach>> tachs;
-  for(auto& tach_path : tach_paths){
-    tachs.emplace_back(std::make_unique<Tach>(tach_path, system_bus, io));
+  int ii = 0;
+  for (auto& fan : fan_json) {
+    for (auto end = ii + tach_per_fan; ii < end; ii++) {
+      std::string name = fan["name"];
+      // todo(james) if we ever have 'c' fans this will need to be revisited
+      if (tach_per_fan > 1) {
+        if (ii % 2)
+          name += "b";
+        else
+          name += "a";
+      }
+      std::cout << name << "\n";
+      tachs.emplace_back(
+          std::make_unique<Tach>(tach_paths.at(ii), system_bus, io, name, fan));
+    }
   }
   io.run();
 }
