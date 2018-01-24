d0795c1fe75e720651947b7d0fa17d05e66f42cb
Feist, James <james.feist@linux.intel.com>
2018-01-24 13:49:23 -0800

    Make entity-cpp run on platform
    Modify setup.py to install only what is needed
    after adding the cpp entity manager. Fix some bugs
    in overlaygen.py to allow for string/unicode buses
    and make the default for entities on.
    
    Change-Id: I6dd47bcd3fb5fd852278858fa9cf11246026e366
    Signed-off-by: Feist, James <james.feist@linux.intel.com>

--- entity-manager/overlay_gen.py
+++ entity-manager/overlay_gen.py
@@ -23,21 +23,26 @@ class Entity(object):
 
     def create_dts(self):
         with open(os.path.join(TEMPLATE_DIR, '{}.template'.format(self.name))) as template_file:
-            output = Template(template_file.read()).substitute(self.template_args)
+            output = Template(template_file.read()).substitute(
+                self.template_args)
 
             outfile = '{}@{}.dts'.format(self.name, self.template_args['name'])
             with open(os.path.join(OUTPUT_DIR, outfile), 'w') as file_handle:
                 file_handle.write(output)
 
     def create_dtbo(self):
-        dts = os.path.join(OUTPUT_DIR, '{}@{}.dts'.format(self.name, self.template_args['name']))
+        dts = os.path.join(
+            OUTPUT_DIR, '{}@{}.dts'.format(self.name, self.template_args['name']))
         dtb = dts.replace('.dts', '.dtbo')
-        subprocess.check_call(r'dtc -@ -I dts -O dtb -o {} {}'.format(dtb, dts), shell=True)
+        subprocess.check_call(
+            r'dtc -@ -I dts -O dtb -o {} {}'.format(dtb, dts), shell=True)
 
     def load_overlay(self):
-        dtb = os.path.join(OUTPUT_DIR, '{}@{}.dtbo'.format(self.name, self.template_args['name']))
+        dtb = os.path.join(OUTPUT_DIR, '{}@{}.dtbo'.format(
+            self.name, self.template_args['name']))
         basename, _ = os.path.splitext(os.path.basename(dtb))
-        subprocess.check_call(r'{} -d {} {}'.format(DTOVERLAY, OUTPUT_DIR, basename), shell=True)
+        subprocess.check_call(r'{} -d {} {}'.format(
+            DTOVERLAY, OUTPUT_DIR, basename), shell=True)
 
     def force_probe(self):
         if not hasattr(self, 'probe_driver'):
@@ -56,6 +61,7 @@ class Entity(object):
 
 
 class TMP75(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'tmp75'
         self.template_args = {'platform': PLATFORM,
@@ -65,6 +71,7 @@ class TMP75(Entity):
 
 
 class TMP421(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'tmp421'
         self.template_args = {'platform': PLATFORM,
@@ -75,6 +82,7 @@ class TMP421(Entity):
 
 
 class EEPROM(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'eeprom'
         self.template_args = {'platform': PLATFORM,
@@ -84,6 +92,7 @@ class EEPROM(Entity):
 
 
 class ADC(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'adc'
         self.template_args = {'platform': PLATFORM,
@@ -92,6 +101,7 @@ class ADC(Entity):
 
 
 class ASPEED_PWMTACHO(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'pwmtach'
         self.probe_driver = r'/sys/bus/platform/drivers/aspeed_pwm_tacho'
@@ -108,6 +118,7 @@ class ASPEED_PWMTACHO(Entity):
 
 
 class ASPEED_PECI_HWMON(Entity):
+
     def __init__(self, **kwargs):
         self.name = 'peci_hwmon'
         self.probe_driver = r'/sys/bus/platform/drivers/aspeed-peci-hwmon'
@@ -127,11 +138,13 @@ def get_obj(**kwargs):
 
 
 def unload_overlays():
-    active = subprocess.check_output(r'{} -d {} -l'.format(DTOVERLAY, OVERLAY_DIR), shell=True)
+    active = subprocess.check_output(
+        r'{} -d {} -l'.format(DTOVERLAY, OVERLAY_DIR), shell=True)
     buf = StringIO.StringIO(active)
     buf.readline()  # first line isn't important
     for _ in buf:
-        subprocess.call(r'{} -d {} -r 0'.format(DTOVERLAY, OUTPUT_DIR), shell=True)
+        subprocess.call(
+            r'{} -d {} -r 0'.format(DTOVERLAY, OUTPUT_DIR), shell=True)
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
 
@@ -148,18 +161,23 @@ if __name__ == '__main__':
         unload_overlays()
     else:
         unload_overlays()
-        configurations = json.load(open(os.path.join(CONFIGURATION_DIR, 'system.json')))
+        configurations = json.load(
+            open(os.path.join(CONFIGURATION_DIR, 'system.json')))
         for _, configuration in configurations.iteritems():
             for element in configuration.get('exposes', []):
                 if not isinstance(
                         element,
                         dict) or element.get(
                     'status',
-                    'disabled') != 'okay':
+                            'okay') != 'okay':
                     continue
                 element['oem_name'] = element.get(
                     'name', 'unknown').replace(
                     ' ', '_')
+
+                bus = element.get('bus', None)
+                if bus and not isinstance(bus, int):
+                    element['bus'] = int(bus, 16)
 
                 # todo, this should really just be a map or deleted
                 if element.get("type", "") == "TMP75":
@@ -168,7 +186,8 @@ if __name__ == '__main__':
 
                 elif element.get("type", "") == "TMP421":
                     element["reg"] = element.get("address").lower()
-                    element["oem_name1"] = element.get("name1").replace(" ", "_")
+                    element["oem_name1"] = element.get(
+                        "name1").replace(" ", "_")
                     load_entity(**element)
 
                 elif element.get("type", "") == "ADC":
--- entity-manager/setup.py
+++ entity-manager/setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 import glob
 
-setup(name='entity-manager',
+setup(name='overlay-generator',
       version='0.1',
 	  packages=['utils'],
-      scripts=['platform_scan.py', 'overlay_gen.py', 'fru_device.py'],
-      data_files=[('overlay_templates', glob.glob('overlay_templates/*')),
-                  ('configurations', glob.glob('configurations/*'))]
+      scripts=['overlay_gen.py'],
+      data_files=[('overlay_templates', glob.glob('overlay_templates/*'))]
       )
