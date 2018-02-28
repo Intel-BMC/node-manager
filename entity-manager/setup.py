73ef8af31515f7d6db0af4fd1189f2b6836d7373
Feist, James <james.feist@linux.intel.com>
2018-02-28 15:53:19 -0800

    Remove entity-manager from proving ground
    There is no functionality used here anymore. Clean up.
    
    Change-Id: Ief43a7d99341e02c93b9703ac2081591e227d7c4
    Signed-off-by: Feist, James <james.feist@linux.intel.com>

--- entity-manager/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-import glob
-
-setup(name='overlay-generator',
-      version='0.1',
-	  packages=['utils'],
-      scripts=['overlay_gen.py'],
-      data_files=[('overlay_templates', glob.glob('overlay_templates/*'))]
-      )
