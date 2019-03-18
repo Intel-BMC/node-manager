c927781ad3ca1408cd7fb0b7cc2538ea10ded76b
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2019-03-19 00:28:30 +0530

    Cleanup: Chassis code moved to intel-chassis-ctrl
    Get rid of dead code as chassis service code in
    proving-ground is no longer used and moved to
    intel-chassis-ctrl (separate-repo)
    
    Change-Id: I4da958ce8f56625969d31339e56c3448585d0cc9
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

--- services/chassis/Buttons/inc/Common.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-struct EventDeleter
-{
-    void operator()(sd_event *event) const
-    {
-        event = sd_event_unref(event);
-    }
-};
-using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
--- services/chassis/Gpio/inc/Gpio.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-int configGpio(const char *gpioName, int *fd, sdbusplus::bus::bus &bus);
-int closeGpio(int fd);
